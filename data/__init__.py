fc031ca7f5d77b29dbc03b030498459263f9bd63
bruce <jingby1994@gmail.com>
2022-12-08 23:12:44 +0800

    update data

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,2 @@
-from dgi_data import DGIData
+from .base_data import BaseData
+from .dgi_data import DGIData
--- data/base_data.py
+++ data/base_data.py
@@ -4,19 +4,22 @@ class BaseData:
     """
     TODO: torch geomtric data structure.
     """
-    def load(self, **kwargs):
+    def load(self, **kwargs) -> None:
         raise NotImplementedError
 
 
 class HomoData(BaseData):
     def __init__(self):
         self.adj = None
-        self.x = None       # node idx
-        self.attrs = None   # node attributes
+        self.attrs = None
 
-    def load(self, path: str):
+    def load(self, path: str) -> None:
         """
         Load the specified dataset.
         path: the folder of the dataset.
         """
         raise NotImplementedError
+
+    @property
+    def n_nodes(self):
+        return len(self.attrs)
