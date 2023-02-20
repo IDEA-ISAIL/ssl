5f5c09828d9de9e4511ad23521d69aa1495f66d0
baoyu jing <jingby1994@gmail.com>
2023-02-20 15:54:15 +0800

    update

--- example.py
+++ example.py
@@ -1,17 +1,16 @@
 from data import DatasetDGI
 from loader import FullLoader
+from augment.collections import augment_dgi
 
-from augment import AugPosDGI, AugNegDGI
-
-from nn.encoders import GCNDGI
 from nn.utils import DiscriminatorDGI
+from nn.encoders import GCNDGI
 from nn.models import ModelDGI
 from methods import DGI
 
 
 # data
 dataset = DatasetDGI()
-dataset.load(path="./datasets/cora_dgi")
+dataset.load(path="datasets/cora_dgi")
 data = dataset.to_data()
 data_loader = FullLoader(data)
 
@@ -22,7 +21,5 @@ model = ModelDGI(encoder=encoder, discriminator=discriminator)
 # model = ModelDGI(encoder=encoder)
 
 # trainer
-augment_pos = AugPosDGI()
-augment_neg = AugNegDGI()
-dgi = DGI(model=model, data_loader=data_loader, augment_pos=AugPosDGI(), augment_neg=AugNegDGI(), save_root="./results")
+dgi = DGI(model=model, data_loader=data_loader, data_augment=augment_dgi, save_root="./results")
 dgi.train()
