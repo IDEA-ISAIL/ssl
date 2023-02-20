163634a991cece63febc4de7b26e4a43f2c5b2d2
baoyu jing <jingby1994@gmail.com>
2023-02-20 17:59:43 +0800

    add utils

--- methods/afgrl.py
+++ methods/afgrl.py
@@ -3,36 +3,13 @@ import torch
 import numpy as np
 from augment import DataAugmentation, AugNegDGI, AugPosDGI
 from loader import Loader, FullLoader
-from .method import ContrastiveMethod
-import copy
+from .base import Method
+from .utils import EMA, update_moving_average
 
 from torch_geometric.typing import *
 
-class EMA:
-    def __init__(self, beta, epochs):
-        super().__init__()
-        self.beta = beta
-        self.step = 0
-        self.total_steps = epochs
 
-    def update_average(self, old, new):
-        if old is None:
-            return new
-        beta = 1 - (1 - self.beta) * (np.cos(np.pi * self.step / self.total_steps) + 1) / 2.0
-        self.step += 1
-        return old * beta + (1 - beta) * new
-
-
-
-def update_moving_average(ema_updater, ma_model, current_model):
-    for current_params, ma_params in zip(current_model.parameters(), ma_model.parameters()):
-        old_weight, up_weight = ma_params.data, current_params.data
-        ma_params.data = ema_updater.update_average(old_weight, up_weight)
-
-
-
-
-class AFGRL(ContrastiveMethod):
+class AFGRL(Method):
     r"""
     TODO: add descriptions
     """
