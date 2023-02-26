55c081a63b8ae13083f1c1c06fea5dc4bd8846b5
Zeng <zhichenz@illinois.edu>
2023-02-25 22:09:48 -0600

    update MVGRL

--- augment/collections.py
+++ augment/collections.py
@@ -3,10 +3,14 @@ import torch
 
 from .base import *
 from .positive import Echo
-from .negative import DataShuffle
+from .negative import DataShuffle, ComputePPR, ComputeHeat
 
 __all__ = [
-    "augment_dgi"
+    "augment_dgi",
+    "augment_mvgrl_ppr",
+    "augment_mvgrl_heat",
 ]
 
 augment_dgi = DataShuffle(is_x=True)
+augment_mvgrl_ppr = ComputePPR()
+augment_mvgrl_heat = ComputeHeat()
