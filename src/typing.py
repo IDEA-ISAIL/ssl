95f064b7949d65d86367b1d194e799e89b255f62
bruce <baoyu.jing@hotmail.com>
2023-03-10 23:19:21 +0800

    update

--- src/typing.py
+++ src/typing.py
@@ -1,7 +1,5 @@
-""" Next update. """
-
-from src.typing import Union, Optional
-from augment import Augmentor, AugmentorList, AugmentorDict
+from typing import Union, Optional
+from src.augment import Augmentor, AugmentorList, AugmentorDict
 
 AugmentType = Union[Augmentor, AugmentorList, AugmentorDict]
 OptAugment = Optional[AugmentType]
