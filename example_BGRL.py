44cc82ec589a7dfbebf65256e7460be07e063e32
weitianxin <771802026@qq.com>
2023-03-13 17:36:18 -0500

    update non_contrast

--- example_BGRL.py
+++ example_BGRL.py
@@ -1,8 +1,9 @@
 from src.data import DatasetDGI
 from src.loader import FullLoader
+from src.augment.collections import augment_bgrl_1, augment_bgrl_2
 
 from src.nn.encoders import GCNDGI
-from src.nn.models import ModelBGRL
+from src.nn.models.bgrl import Model
 from src.methods import BGRL
 import copy
 
@@ -16,9 +17,9 @@ data_loader = FullLoader(data)
 # TODO: use GCNDGI for now
 student_encoder = GCNDGI(dim_in=1433)
 teacher_encoder = copy.deepcopy(student_encoder)
-model = ModelBGRL(student_encoder=student_encoder, teacher_encoder=teacher_encoder)
+model = Model(student_encoder=student_encoder, teacher_encoder=teacher_encoder)
 
 # trainer
 #TODO: add augmentation
-bgrl = BGRL(model=model, data_loader=data_loader, save_root="./results")
+bgrl = BGRL(model=model, data_loader=data_loader, save_root="./results",  data_augment_1 = augment_bgrl_1, data_augment_2 = augment_bgrl_2)
 bgrl.train()
