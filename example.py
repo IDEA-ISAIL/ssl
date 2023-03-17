8b7372f6f443746a8d88a46ebf70f52195dc4227
bruce <baoyu.jing@hotmail.com>
2023-03-17 20:41:07 +0800

    try both fully and partially torch_geometric implementation

--- example.py
+++ example.py
@@ -1,15 +1,17 @@
-from src.data import DatasetDGI
-from src.augment import ShuffleNode
 import torch
+from torch_geometric.nn.models import GCN
+from torch_geometric.loader import DataLoader
 
-from src.methods import DGI
-from src.methods.utils import DGIDiscriminator
-from src.trainer import SimpleTrainer
+from src.data import DatasetDGI
 from src.datasets import Planetoid
 from src.transforms import NormalizeFeatures, GCNNorm, Edge2Adj, Compose
 
-from torch_geometric.nn.models import GCN
-from torch_geometric.loader import DataLoader
+from src.methods import DGI
+from src.methods.utils import DGIDiscriminator, DGIGCN
+from src.trainer import SimpleTrainer
+from src.augment import ShuffleNode
+
+from src.evaluation import LogisticRegression
 
 # -----------------------------------------
 # Show the difference between current & old versions.
@@ -29,22 +31,41 @@ print("Attribute difference:", torch.sum(x_pyg - x_old))
 print("Adjacency matrix difference:", torch.sum(adj_pyg - adj_old))
 # ------------------------------------------
 
+train_mask = data_pyg.train_mask
+print(train_mask.shape)
+
 data_loader = DataLoader(dataset)
 
+# ---------------- Fully Torch_geometric -------------------
 # Neural networks
-# encoder = Encoder(dim_in=1433)
-encoder = GCN(in_channels=1433, hidden_channels=512, num_layers=1, act="prelu")
+encoder = GCN(in_channels=1433, hidden_channels=512, num_layers=1, act="prelu")  # torch_geometric build-in GCN
 discriminator = DGIDiscriminator(in_channels=512)
-method = DGI(encoder=encoder, data_augment=ShuffleNode, discriminator=discriminator)
+method = DGI(encoder=encoder, data_augment=ShuffleNode(), discriminator=discriminator, conv_type="spatial")
 
 # Trainer
-trainer = SimpleTrainer(method=method, data_loader=data_loader)
+trainer = SimpleTrainer(method=method, data_loader=data_loader, device="cuda:0")
+trainer.train()
 
-# #evaluator
-# embs = model.get_embs(x=data.x.cuda(), adj=data.adj.cuda(), is_numpy=True)
-# labels = dataset.labels
-#
-# # node classification/ link prediction/ graph reconstruction
-#
-# eval(embs, labels, evaluator = "similarity_search")
+# Evaluation
+data_pyg = data_pyg.to(method.device)
+embs = method.get_embs(x=data_pyg.x, edge_index=data_pyg.edge_index, is_numpy=False)
 
+lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device="cuda")
+lg(embs=embs, dataset=data_pyg)
+
+# # ----------------- Partially Torch_geometric -----------------------
+# Neural Network
+encoder = DGIGCN(in_channels=1433, hidden_channels=512)  # the GCN implemented by DGI
+discriminator = DGIDiscriminator(in_channels=512)
+method = DGI(encoder=encoder, data_augment=ShuffleNode(), discriminator=discriminator, conv_type="spectral")
+
+# Trainer
+trainer = SimpleTrainer(method=method, data_loader=data_loader, device="cuda:0")
+trainer.train()
+
+# Evaluation
+data_pyg = data_pyg.to(method.device)
+embs = method.get_embs(x=data_pyg.x, adj=data_pyg.adj_t.to_torch_sparse_coo_tensor(), is_numpy=False)
+
+lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device="cuda")
+lg(embs=embs, dataset=data_pyg)
