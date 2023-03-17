0a9a72c14da133e5c52994598047787283f70839
bruce <baoyu.jing@hotmail.com>
2023-03-17 20:38:32 +0800

    add DGIGCN

--- src/methods/utils/__init__.py
+++ src/methods/utils/__init__.py
@@ -1,4 +1,5 @@
 from .dgi_discriminator import DGIDiscriminator
+from .dgi_gcn import DGIGCN
 from .get_label_paris import get_label_pairs
 from .ema import EMA, update_moving_average
 from .readout import AvgReadout
