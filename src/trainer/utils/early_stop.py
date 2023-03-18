8f7d9484f44c840f9f1eb2eb18c07c32a2d9d750
baoyu jing <jingby1994@gmail.com>
2023-03-18 11:49:20 +0800

    update

--- src/trainer/utils/early_stop.py
+++ src/trainer/utils/early_stop.py
@@ -1,18 +1,21 @@
 class EarlyStopper:
-    def __init__(self, patience, best_value):
+    def __init__(self, patience):
         self.patience = patience
-        self.best_value = best_value
 
-        self.n_wait = 0
+        self._best_value = None
+        self._n_wait = 0
 
     def is_stop(self, current_value):
-        if current_value < self.best_value:
-            self.best_value = current_value
-            self.n_wait = 0
-            return False
+        if self._best_value is None:
+            self._best_value = current_value
+            self._n_wait = 0
         else:
-            self.n_wait += 1
+            if current_value < self._best_value:
+                self._n_wait = 0
+                return False
+            else:
+                self._n_wait += 1
 
-        if self.n_wait == self.patience:
+        if self._n_wait == self.patience:
             print('Early stopped!')
             return True
