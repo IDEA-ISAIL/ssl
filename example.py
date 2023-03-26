680ae6b52b87b1d67c928214403573f3e03a30ef
bruce <baoyu.jing@hotmail.com>
2023-03-26 22:02:07 +0800

    update

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ from src.datasets import Planetoid
 from src.transforms import NormalizeFeatures, GCNNorm, Edge2Adj, Compose
 
 from src.methods import DGI
-from src.methods.utils import DGIDiscriminator, DGIGCN
+# from src.methods.utils import DGIDiscriminator, DGIGCN
 from src.trainer import SimpleTrainer
 from src.augment import ShuffleNode
 
@@ -35,20 +35,8 @@ data_loader = DataLoader(dataset)
 
 # ---------------- Fully Torch_geometric -------------------
 # Neural networks
-class Encoder(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels=512, num_layers=1, act=torch.nn.PReLU()):
-        super().__init__()
-        # torch_geometric build-in GCN
-        self.gcn = GCN(in_channels=in_channels, hidden_channels=hidden_channels, num_layers=num_layers, act=act)
-        self.act = act
 
-    def forward(self, x, edge_index, edge_weight=None):
-        return self.act(self.gcn(x=x, edge_index=edge_index, edge_weight=edge_weight))
-
-
-encoder = Encoder(in_channels=1433)
-discriminator = DGIDiscriminator(in_channels=512)
-method = DGI(encoder=encoder, data_augment=ShuffleNode(), discriminator=discriminator, conv_type="spatial")
+method = DGI(in_channels=1433, hidden_channels=512)
 
 # Trainer
 trainer = SimpleTrainer(method=method, data_loader=data_loader, device="cuda:0")
@@ -61,19 +49,46 @@ embs = method.get_embs(x=data_pyg.x, edge_index=data_pyg.edge_index, is_numpy=Fa
 lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device="cuda")
 lg(embs=embs, dataset=data_pyg)
 
-# # ----------------- Partially Torch_geometric -----------------------
-# Neural Network
-encoder = DGIGCN(in_channels=1433, hidden_channels=512)  # the GCN implemented by DGI
-discriminator = DGIDiscriminator(in_channels=512)
-method = DGI(encoder=encoder, data_augment=ShuffleNode(), discriminator=discriminator, conv_type="spectral")
+# # ---------------- Fully Torch_geometric -------------------
+# # Neural networks
+# class Encoder(torch.nn.Module):
+#     def __init__(self, in_channels, hidden_channels=512, num_layers=1, act=torch.nn.PReLU()):
+#         super().__init__()
+#         # torch_geometric build-in GCN
+#         self.gcn = GCN(in_channels=in_channels, hidden_channels=hidden_channels, num_layers=num_layers, act=act)
+#         self.act = act
+#
+#     def forward(self, x, edge_index, edge_weight=None):
+#         return self.act(self.gcn(x=x, edge_index=edge_index, edge_weight=edge_weight))
+#
+#
+# encoder = Encoder(in_channels=1433)
+# method = DGI(encoder=encoder, data_augment=ShuffleNode())
+#
+# # Trainer
+# trainer = SimpleTrainer(method=method, data_loader=data_loader, device="cuda:0")
+# trainer.train()
+#
+# # Evaluation
+# data_pyg = data_pyg.to(method.device)
+# embs = method.get_embs(x=data_pyg.x, edge_index=data_pyg.edge_index, is_numpy=False)
+#
+# lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device="cuda")
+# lg(embs=embs, dataset=data_pyg)
 
-# Trainer
-trainer = SimpleTrainer(method=method, data_loader=data_loader, device="cuda:0")
-trainer.train()
-
-# Evaluation
-data_pyg = data_pyg.to(method.device)
-embs = method.get_embs(x=data_pyg.x, adj=data_pyg.adj_t.to_torch_sparse_coo_tensor(), is_numpy=False)
-
-lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device="cuda")
-lg(embs=embs, dataset=data_pyg)
+# # # ----------------- Partially Torch_geometric -----------------------
+# # Neural Network
+# encoder = DGIGCN(in_channels=1433, hidden_channels=512)  # the GCN implemented by DGI
+# discriminator = DGIDiscriminator(in_channels=512)
+# method = DGI(encoder=encoder, data_augment=ShuffleNode(), discriminator=discriminator, conv_type="spectral")
+#
+# # Trainer
+# trainer = SimpleTrainer(method=method, data_loader=data_loader, device="cuda:0")
+# trainer.train()
+#
+# # Evaluation
+# data_pyg = data_pyg.to(method.device)
+# embs = method.get_embs(x=data_pyg.x, adj=data_pyg.adj_t.to_torch_sparse_coo_tensor(), is_numpy=False)
+#
+# lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device="cuda")
+# lg(embs=embs, dataset=data_pyg)
--- src/methods/dgi.py
+++ src/methods/dgi.py
@@ -1,76 +1,73 @@
 import torch
+from torch_geometric.nn.models import GCN
 
 from .base import BaseMethod
+from .utils import AvgReadout
 from src.augment import ShuffleNode
-from .utils import get_label_pairs, AvgReadout
+from src.losses import NegativeMI
 
-from typing import Callable, Union
+from typing import Optional
 from src.typing import AugmentType
-
-
-CONV_TYPES = ['spectral', 'spatial', 0, 1]
 
 
 class DGI(BaseMethod):
     r""" Deep Graph Infomax (DGI).
 
     Args:
-        encoder (torch.nn.Module): the encoder to be trained.
-
-        discriminator (torch.nn.Module): the discriminator to discriminate positive and negative node pairs.
-
-        data_augment (AugmentType): data augmentation to generate negative node pairs. (default: ShuffleNode()).
-
-        loss_function (Callable): the loss function. (default: torch.nn.BCEWithLogitsLoss())
-
-        conv_type (Union[str, int]): 'spectral' (0) or 'spatial' (1).
-            'spectral': spectral convolution.
-                It updates node embeddings by AXW.
-                The inputs are the feature matrix 'x' & adjacency matrix 'adj'.
-            'spatial': spatial convolution.
-                It updates node embeddings by message passing (the built-in GCN).
-                The inputs are the feature matrix 'x' & 'edge_index' & 'edge_weight'.
-            (default: 'spectral')
+        in_channels (Optional[int]): the input channels of the encoder. Must be specified if use the default "encoder".
+        hidden_channels (Optional[int]): the hidden channels of the encoder. Must be specified if use the default
+            "encoder" and "loss_function".
+        encoder (Optional[torch.nn.Module]): the encoder to be trained. If use the default encoder, must specify
+            "in_channels" and "hidden_channels".
+        data_augment (AugmentType): data augmentation to generate negative node pairs.
+            (default: ShuffleNode).
+        loss_function (Callable): the loss function.
+            (default: NegativeMI)
     """
     def __init__(self,
-                 encoder: torch.nn.Module,
-                 discriminator: torch.nn.Module,
+                 in_channels: Optional[int] = None,
+                 hidden_channels: Optional[int] = None,
+                 encoder: Optional[torch.nn.Module] = None,
+                 readout: Optional[torch.nn.Module] = None,
                  data_augment: AugmentType = ShuffleNode(),
-                 loss_function: Callable = torch.nn.BCEWithLogitsLoss(),
-                 conv_type: Union[str, int] = 'spectral') -> None:
-        """
-        TODO: do we need "int" for conv_type?
-        """
+                 loss_function: Optional[torch.nn.Module] = None) -> None:
         super().__init__(encoder=encoder, data_augment=data_augment, loss_function=loss_function)
 
-        self.discriminator = discriminator
-        self.read = AvgReadout()
+        if self.encoder is None:
+            assert in_channels > 0 and hidden_channels > 0,\
+                "If use the default GCN encoder, then in_channels and hidden_channels must be set."
+            self.encoder = Encoder(in_channels=in_channels, hidden_channels=hidden_channels)
+
+        if self.loss_function is None:
+            assert hidden_channels > 0, \
+                "If use the default loss function (negative mutual information), then hidden_channels must be set."
+            self.loss_function = NegativeMI(in_channels=hidden_channels)
+
+        self.readout = readout if readout else AvgReadout()
         self.sigmoid = torch.nn.Sigmoid()
 
-        assert conv_type in CONV_TYPES
-        self.conv_type = conv_type
-
     def forward(self, batch, batch_neg):
-        h_1 = self.encoder_switch(batch)
-        c = self.read(h_1)
-        c = self.sigmoid(c)
+        edge_weight = batch.edge_weight if "edge_weight" in batch else None
+        h_pos = self.encoder(x=batch.x, edge_index=batch.edge_index, edge_weight=edge_weight)
+        h_neg = self.encoder(x=batch_neg.x, edge_index=batch.edge_index, edge_weight=edge_weight)
 
-        h_2 = self.encoder_switch(batch_neg)
-
-        logits = self.discriminator(c, h_1, h_2)
-        return logits
+        s = self.readout(h_pos, keepdim=True)
+        s = self.sigmoid(s)
+        return s, h_pos, h_neg
 
     def train_iter(self, batch):
         batch_neg = self.data_augment(batch).to(self._device)
-        logits = self.forward(batch=batch, batch_neg=batch_neg)
-        labels = get_label_pairs(n_pos=batch.num_nodes, n_neg=batch.num_nodes).to(self._device)
-        loss = self.loss_function(logits, labels)
+        s, h_pos, h_neg = self.forward(batch=batch, batch_neg=batch_neg)
+        s = s.expand_as(h_pos)
+        loss = self.loss_function(x=s, y=h_pos, x_ind=s, y_ind=h_neg)
         return loss
 
-    def encoder_switch(self, batch):
-        if self.conv_type == "spectral" or self.conv_type == 0:
-            h = self.encoder(x=batch.x, adj=batch.adj_t.to_torch_sparse_coo_tensor())
-        if self.conv_type == "spatial" or self.conv_type == 1:
-            edge_weight = batch.edge_weight if "edge_weight" in batch else None
-            h = self.encoder(x=batch.x, edge_index=batch.edge_index, edge_weight=edge_weight)
-        return h
+
+class Encoder(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels=512, num_layers=1, act=torch.nn.PReLU()):
+        super().__init__()
+        self.gcn = GCN(in_channels=in_channels, hidden_channels=hidden_channels, num_layers=num_layers, act=act)
+        self.act = act
+
+    def forward(self, x, edge_index, edge_weight=None):
+        return self.act(self.gcn(x=x, edge_index=edge_index, edge_weight=edge_weight))
