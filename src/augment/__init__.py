d4389629f94ddf6fe7c3891a895b8a4d37f1ac6d
bruce <baoyu.jing@hotmail.com>
2023-03-27 23:28:18 +0800

    add echo

--- src/augment/__init__.py
+++ src/augment/__init__.py
@@ -1,6 +1,8 @@
 from .base import Augmentor, AugmentorList, AugmentorDict
 from .collections import *
+from .echo import Echo
 from .shuffle_node import ShuffleNode
+
 
 # __all__ = [
 #     "Augmentation",
