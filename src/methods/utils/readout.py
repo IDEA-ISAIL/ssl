66bea696379dbd47d78e1c864c931db5d3d0d93f
bruce <baoyu.jing@hotmail.com>
2023-03-27 22:22:56 +0800

    update

--- src/methods/utils/readout.py
+++ src/methods/utils/readout.py
@@ -1,12 +1,10 @@
 import torch
-
 from torch_geometric.typing import Tensor
 
 
-class AvgReadout(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
+class AvgReadout(object):
+    def __init__(self) -> None:
+        return
 
-    def forward(self, x: Tensor, keepdim: bool = False):
+    def __call__(self, x: Tensor, keepdim: bool = False):
         return torch.mean(x, -2, keepdim=keepdim)
-
