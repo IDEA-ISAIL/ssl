9e435849754bc4916524182e9e6601932217a5bc
Lecheng Zheng <33203374+Leo02016@users.noreply.github.com>
2024-03-23 20:22:54 -0500

    Update example_graphcl.py

--- example_graphcl.py
+++ example_graphcl.py
@@ -1,19 +1,47 @@
 from src.augment import RandomMask, RandomDropEdge, RandomDropNode, AugmentSubgraph, AugmentorList
-from src.methods import GraphCL, GraphCLEncoder
+from src.methods.infograph import InfoGraph, Encoder
 from src.trainer import SimpleTrainer
 from torch_geometric.loader import DataLoader
-from src.transforms import NormalizeFeatures, GCNNorm, Edge2Adj, Compose
-from src.datasets import Planetoid, Entities
+from torch_geometric.datasets import TUDataset
 from src.evaluation import LogisticRegression
+from src.config import load_yaml
+from torch_geometric.nn import GINConv
 import torch
+import os
+import numpy as np
 
 
 torch.manual_seed(0)
+config = load_yaml('./configuration/infograph_mutag.yml')
+# config = load_yaml('./configuration/infograph_imdb_b.yml')
+# config = load_yaml('./configuration/infograph_imdb_m.yml')
+torch.manual_seed(config.torch_seed)
+np.random.seed(config.torch_seed)
+device = torch.device("cuda:{}".format(config.gpu_idx) if torch.cuda.is_available() and config.use_cuda else "cpu")
+
 # data
-pre_transforms = Compose([NormalizeFeatures(ord=1), Edge2Adj(norm=GCNNorm(add_self_loops=1))])
-# dataset = Planetoid(root="pyg_data", name="cora", pre_transform=pre_transforms)
-dataset = Entities(root="pyg_data", name="mutag", pre_transform=pre_transforms)
-data_loader = DataLoader(dataset)
+# -------------------- Data --------------------
+print(os.path.dirname(os.path.realpath(__file__)))
+current_folder = os.path.abspath('')
+print(current_folder)
+path = os.path.join(os.path.dirname(os.path.realpath(__file__)), config.dataset.root, config.dataset.name)
+if config.dataset.name in ['IMDB-B', 'IMDB-M', 'mutag', 'COLLAB', 'PROTEINS']:
+    # dataset = TUDataset(path, name=config.dataset.name).shuffle()
+    dataset = TUDataset(path, name=config.dataset.name)
+else:
+    raise NotImplementedError
+# dataset.x = torch.rand(dataset.y.shape[0], 100)
+data_loader = DataLoader(dataset, batch_size=config.dataset.batch_size)
+
+in_channels = max(dataset.num_features, 1)
+
+### to handle molecular graph, graphcl use infograph as the backbone. ###
+# ------------------- Method -----------------
+#
+encoder = Encoder(in_channels=in_channels, hidden_channels=config.model.hidden_channels,
+                  num_layers=config.model.n_layers, GNN=GINConv)
+method = InfoGraph(encoder=encoder, hidden_channels=config.model.hidden_channels, num_layers=config.model.n_layers,
+                   prior=False)
 
 # Augmentation
 aug_type = 'mask'
@@ -30,20 +58,23 @@ else:
 augment_neg = AugmentorList([RandomDropEdge(), RandomMask()])
 
 
-# ------------------- Method -----------------
-encoder = GraphCLEncoder(in_channels=1433, hidden_channels=512)
-method = GraphCL(encoder=encoder, corruption=augment_neg, hidden_channels=512)
-method.augment_type = aug_type
-
-
 # ------------------ Trainer --------------------
 trainer = SimpleTrainer(method=method, data_loader=data_loader, device="cuda:0")
 trainer.train()
 
 
 # ------------------ Evaluator -------------------
+method.eval()
 data_pyg = dataset.data.to(method.device)
-embs = method.get_embs(data_pyg, data_pyg.adj_t).detach()
+y, embs = method.get_embs(data_loader)
 
-lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device="cuda")
+data_pyg.x = embs
+lg = LogisticRegression(lr=config.classifier.base_lr, weight_decay=config.classifier.weight_decay,
+                        max_iter=config.classifier.max_epoch, n_run=1, device=device)
 lg(embs=embs, dataset=data_pyg)
+
+
+
+
+
+
