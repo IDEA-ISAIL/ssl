4898b2b22a759fcaba47d2d66e1aa7a2c564a1c3
Zeng <zhichenz@illinois.edu>
2024-09-07 16:48:53 -0500

    fix bug in mvgrl example

--- example_mvgrl.py
+++ example_mvgrl.py
@@ -11,16 +11,30 @@ from src.utils.create_data import create_masks
 from src.utils.add_adj import add_adj_t
 
 # load the configuration file
-config = load_yaml('./configuration/mvgrl_cora.yml')
+config = load_yaml('./configuration/mvgrl_wikics.yml')
 torch.manual_seed(config.torch_seed)
 device = torch.device("cuda:{}".format(config.gpu_idx) if torch.cuda.is_available() and config.use_cuda else "cuda")
 
 
-
 # -------------------- Data --------------------
-pre_transforms = Compose([NormalizeFeatures(ord=1), Edge2Adj(norm=GCNNorm(add_self_loops=1))])
-dataset = Planetoid(root="pyg_data", name="cora", pre_transform=pre_transforms)
-data_loader = DataLoader(dataset)
+if config.dataset.name == 'cora':
+    pre_transforms = Compose([NormalizeFeatures(ord=1), Edge2Adj(norm=GCNNorm(add_self_loops=1))])
+    dataset = Planetoid(root='pyg_data', name='cora', pre_transform=pre_transforms)
+elif config.dataset.name == 'Amazon':
+    pre_transforms = NormalizeFeatures(ord=1)
+    dataset = Amazon(root='pyg_data', name='Photo', pre_transform=pre_transforms)
+elif config.dataset.name == 'WikiCS':
+    pre_transforms = NormalizeFeatures(ord=1)
+    dataset = WikiCS(root='pyg_data', pre_transform=pre_transforms)
+elif config.dataset.name == 'coauthor':
+    pre_transforms = NormalizeFeatures(ord=1)
+    dataset = Coauthor(root='pyg_data', name='CS', pre_transform=pre_transforms)
+else:
+    raise 'please specify the correct dataset root'
+if config.dataset.name in ['Amazon', 'WikiCS', 'coauthor']:
+    dataset.data = create_masks(dataset.data, config.dataset.name)
+dataset = add_adj_t(dataset)
+data_loader = DataLoader(dataset, batch_size=config.model.batch_size)
 
 
 # ------------------- Method -----------------
@@ -40,6 +54,6 @@ enc_embs = method.encoder(data_pyg.x, data_neg.x, data_pyg.adj_t, data_neg.adj_t
 embs = enc_embs['final']
 
 
-lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device="cuda")
+lg = LogisticRegression(lr=0.01, weight_decay=0, max_iter=100, n_run=50, device=device)
 lg(embs=embs, dataset=data_pyg)
 
